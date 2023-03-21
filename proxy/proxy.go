788f9e86984a29c08de928a859845ee45ed6209d
ch0ngsheng <mail.chongsheng@gmail.com>
2023-03-21 17:51:12 +0800

    移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -32,8 +32,8 @@ func modifyRequest(req *http.Request, targetHost string) {
 	req.Host = targetHost
 	req.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41")
 
-	req.Header.Del("x-forwarded-for")
-	req.Header.Del("x-real-ip")
+	req.Header["X-Forwarded-For"] = nil
+	req.Header.Del("X-Real-Ip")
 }
 
 func errorHandler() func(http.ResponseWriter, *http.Request, error) {
