74f67b1b3541b9c070aea717dde3e03ab8eaadc6
ch0ngsheng <mail.chongsheng@gmail.com>
2023-03-21 19:36:42 +0800

    update ua

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -31,7 +31,7 @@ func New(targetHost string) (*httputil.ReverseProxy, error) {
 
 func modifyRequest(req *http.Request, targetHost string) {
 	req.Host = targetHost
-	req.Header.Set("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41")
+	req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
 
 	req.Header["X-Forwarded-For"] = nil
 	req.Header.Del("X-Real-Ip")
